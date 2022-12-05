305958f9a3ebcce82da839f24a23e3bd2622825c
Intel i9 <mtrullasp@gmail.com>
2022-12-05 09:43:26 +0100

    Prmer commit

---
+++ frmOmple.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmOmple
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.bt = New MaterialSkin.Controls.MaterialFlatButton()
+        Me.lb1 = New MaterialSkin.Controls.MaterialLabel()
+        Me.lb2 = New MaterialSkin.Controls.MaterialLabel()
+        Me.pb1 = New PictureBoxExtended.PictureBoxExtended()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.pb2 = New PictureBoxExtended.PictureBoxExtended()
+        Me.lb3 = New MaterialSkin.Controls.MaterialLabel()
+        Me.MaterialLabel1lb4 = New MaterialSkin.Controls.MaterialLabel()
+        Me.TableLayoutPanel1.SuspendLayout()
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.bt, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.lb1, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.lb2, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.pb1, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.ProgressBar1, 0, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.pb2, 1, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.lb3, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.MaterialLabel1lb4, 1, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 5
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(2810, 1457)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'bt
+        '
+        Me.bt.AutoSize = True
+        Me.bt.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.TableLayoutPanel1.SetColumnSpan(Me.bt, 2)
+        Me.bt.Depth = 0
+        Me.bt.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.bt.Location = New System.Drawing.Point(4, 6)
+        Me.bt.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.bt.MouseState = MaterialSkin.MouseState.HOVER
+        Me.bt.Name = "bt"
+        Me.bt.Primary = False
+        Me.bt.Size = New System.Drawing.Size(2802, 88)
+        Me.bt.TabIndex = 0
+        Me.bt.Text = "Omple i Verifica la BD p2003"
+        Me.bt.UseVisualStyleBackColor = True
+        '
+        'lb1
+        '
+        Me.lb1.AutoSize = True
+        Me.lb1.Depth = 0
+        Me.lb1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lb1.Font = New System.Drawing.Font("Roboto", 11.0!)
+        Me.lb1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lb1.Location = New System.Drawing.Point(3, 150)
+        Me.lb1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.lb1.Name = "lb1"
+        Me.lb1.Size = New System.Drawing.Size(1399, 50)
+        Me.lb1.TabIndex = 1
+        Me.lb1.Text = "MaterialLabel1"
+        '
+        'lb2
+        '
+        Me.lb2.AutoSize = True
+        Me.lb2.Depth = 0
+        Me.lb2.Font = New System.Drawing.Font("Roboto", 11.0!)
+        Me.lb2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lb2.Location = New System.Drawing.Point(1408, 150)
+        Me.lb2.MouseState = MaterialSkin.MouseState.HOVER
+        Me.lb2.Name = "lb2"
+        Me.lb2.Size = New System.Drawing.Size(266, 46)
+        Me.lb2.TabIndex = 2
+        Me.lb2.Text = "MaterialLabel2"
+        '
+        'pb1
+        '
+        Me.pb1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pb1.Location = New System.Drawing.Point(3, 203)
+        Me.pb1.Name = "pb1"
+        Me.pb1.Size = New System.Drawing.Size(1399, 1201)
+        Me.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pb1.TabIndex = 3
+        Me.pb1.TabStop = False
+        Me.pb1.WaitOnLoad = True
+        '
+        'ProgressBar1
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.ProgressBar1, 2)
+        Me.ProgressBar1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ProgressBar1.Location = New System.Drawing.Point(3, 1410)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(2804, 44)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'pb2
+        '
+        Me.pb2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pb2.Location = New System.Drawing.Point(1408, 203)
+        Me.pb2.Name = "pb2"
+        Me.pb2.Size = New System.Drawing.Size(1399, 1201)
+        Me.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pb2.TabIndex = 6
+        Me.pb2.TabStop = False
+        Me.pb2.WaitOnLoad = True
+        '
+        'lb3
+        '
+        Me.lb3.AutoSize = True
+        Me.lb3.Depth = 0
+        Me.lb3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lb3.Font = New System.Drawing.Font("Roboto", 11.0!)
+        Me.lb3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lb3.Location = New System.Drawing.Point(3, 100)
+        Me.lb3.MouseState = MaterialSkin.MouseState.HOVER
+        Me.lb3.Name = "lb3"
+        Me.lb3.Size = New System.Drawing.Size(1399, 50)
+        Me.lb3.TabIndex = 7
+        Me.lb3.Text = "lb3"
+        '
+        'MaterialLabel1lb4
+        '
+        Me.MaterialLabel1lb4.AutoSize = True
+        Me.MaterialLabel1lb4.Depth = 0
+        Me.MaterialLabel1lb4.Font = New System.Drawing.Font("Roboto", 11.0!)
+        Me.MaterialLabel1lb4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialLabel1lb4.Location = New System.Drawing.Point(1408, 100)
+        Me.MaterialLabel1lb4.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialLabel1lb4.Name = "MaterialLabel1lb4"
+        Me.MaterialLabel1lb4.Size = New System.Drawing.Size(71, 46)
+        Me.MaterialLabel1lb4.TabIndex = 8
+        Me.MaterialLabel1lb4.Text = "lb4"
+        '
+        'frmOmple
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(2810, 1457)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "frmOmple"
+        Me.Text = "Omple BD p2023"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents bt As MaterialSkin.Controls.MaterialFlatButton
+    Friend WithEvents lb1 As MaterialSkin.Controls.MaterialLabel
+    Friend WithEvents lb2 As MaterialSkin.Controls.MaterialLabel
+    Friend WithEvents pb1 As PictureBoxExtended.PictureBoxExtended
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents pb2 As PictureBoxExtended.PictureBoxExtended
+    Friend WithEvents lb3 As MaterialSkin.Controls.MaterialLabel
+    Friend WithEvents MaterialLabel1lb4 As MaterialSkin.Controls.MaterialLabel
+End Class
---
+++ frmOmple.vb
@@ -0,0 +1,373 @@
+﻿Imports Classica.Api
+Imports Classica.Api.ObjAlbum
+Imports Classica.Data.Repository2003
+Imports Classica.Presto.Web
+Imports ObjPrestoWeb
+Imports FGS.Linq.Extensions.EntityFramework
+Imports FGS.Linq.Extensions.Pagination
+
+Public Class frmOmple
+
+    Property mns As New List(Of String)
+
+    'Property Titol = "Sobrevolar TPerformers"
+    Property Titol = "Omplir TPersona"
+    Property albumsFets As Integer = 0
+    Property tracksfets As Integer = 0
+    Property Objdb As New DbContextP2003
+    Property db As p2023Entities = Objdb.db
+
+    Private Sub OmplirTPersona()
+        Using Objdb As New DbContextP2003
+            Dim db = Objdb.db
+            Dim ut As New ObjUtil.ObjUtil
+            Dim composers = (From t In db.TComposer
+                             Where t.ranking > 0
+                             Select t Order By t.ranking).ToList
+
+            For Each one In composers
+                lb1.Text = one.name
+                OmplirTPersonesFromComposerOne(one)
+                DE()
+            Next
+        End Using
+    End Sub
+
+    Sub OmplirTPersonesFromComposerOne(one As TComposer)
+        Dim fadies = Now.AddDays(5)
+        If one.dateProcess IsNot Nothing AndAlso one.dateProcess > fadies Then
+            Exit Sub
+        End If
+        Dim obj As New ObjWebScrap.ObjAM
+        obj.Msg = Sub(msg As String)
+                      lb3.Text = msg
+                      'System.Threading.Thread.Sleep(500)
+                      DE()
+                  End Sub
+
+        obj.MsgImatge = Sub(img As Image)
+                            pb1.Image = img
+                            DE()
+                        End Sub
+
+        obj.MsgUpdated = Sub(persona As TPersona)
+                             If mns.Contains(persona.idMN) Then
+                                 Exit Sub
+                             End If
+                             mns.Add(persona.idMN)
+                             If persona.dataProces Is Nothing OrElse persona.dataProces < fadies Then
+                                 lb3.Text = "Crida ProcesBio(obj, idPresto, persona, persona.bio)" & ":" & one.id
+                                 DE()
+                                 ProcesBio(obj, one.id, persona, persona.bio)
+                             End If
+                         End Sub
+
+        lb3.Text = "Crida ObjWebScrap.ObjAM" & ":" & one.id
+        DE()
+        obj.SearchByArtistName(one.name)
+    End Sub
+
+    Function GetAllMnLinksFromBio(bio As String) As List(Of String)
+        Dim resp As New List(Of String)
+        Dim rest = bio
+        Do Until rest = ""
+            Dim p1 = rest.IndexOf("-mn")
+            If p1 < 0 Then
+                rest = ""
+                Continue Do
+            End If
+            resp.Add(rest.Substring(p1 + 1, 12))
+            rest = rest.Substring(p1 + 12)
+        Loop
+        Return resp
+    End Function
+
+    Sub ProcesBio(obj As ObjWebScrap.ObjAM, idpresto As Long, persona As TPersona, bio As String)
+        Dim ll = GetAllMnLinksFromBio(bio)
+        For Each l In ll
+            obj.DoOneIdMN(l, False)
+        Next
+    End Sub
+
+    Private Sub SobreVolarTPersones()
+        Using Objdb As New DbContextP2003
+            Dim db = Objdb.db
+            Dim ut As New ObjUtil.ObjUtil
+            Dim persones = (From t In db.TPersona
+                            Order By t.imgBase64 Is Nothing
+                            Select t).Take(10000)
+            For Each one In persones
+                bt.Text = one.name
+                pb1.Image = ut.Base64ToImage(one.imgBase64)
+                System.Threading.Thread.Sleep(600)
+                DE()
+            Next
+        End Using
+    End Sub
+
+    Sub DoClick(sender As Object, e As EventArgs) Handles bt.Click
+        bt.Enabled = False
+        'SobreVolarTPersones()
+        OmplirTPersona()
+        bt.Enabled = True
+        DE()
+    End Sub
+
+    Private Sub VerificaAlbums_Click(sender As Object, e As EventArgs)
+        bt.Enabled = False
+        VerificaAlbums()
+        bt.Enabled = True
+        DE()
+    End Sub
+
+    Sub OmpleLabels()
+        Using db As New DbContextP2003
+            Dim presto As New ObjPrestoWeb()
+            presto.MsgImatge1 = Sub(img As Image)
+                                    pb1.Image = img
+                                End Sub
+            presto.DoLoopLabelsPopulars()
+        End Using
+    End Sub
+
+    Private Sub AddAlbumsFromArtists_Click(sender As Object, e As EventArgs)
+        bt.Enabled = False
+        OmpleAlbumsByArtistsNoTracks()
+        bt.Enabled = True
+        DE()
+    End Sub
+
+    Private Sub AlbumsPerPerformer_Click(sender As Object, e As EventArgs)
+        bt.Enabled = False
+        AlbumsPerformers()
+        bt.Enabled = True
+        DE()
+    End Sub
+
+    Private Sub OmpleLabels_Click(sender As Object, e As EventArgs)
+        bt.Enabled = False
+        OmpleLabels()
+        bt.Enabled = True
+        DE()
+    End Sub
+
+    Private Sub SetLabelsToAlbums_Click(sender As Object, e As EventArgs)
+        bt.Enabled = False
+        SetLabelToAlbums()
+        bt.Enabled = True
+        DE()
+    End Sub
+
+
+    Sub OmpleAlbumsByArtistsNoTracks()
+        'TOT el pipeline 
+        Using Objdb As New DbContextP2003
+            Dim db = Objdb.db
+            Dim ss = From t In db.TAlbum
+                     Where t.processDate Is Nothing And Not db.TTrack.Any(Function(a) a.idAlbum = t.id)
+
+            ProgressBar1.Maximum = ss.Count
+
+            Dim fets As Boolean = False
+            Do
+                'Try
+                OmpleAlbumsByArtistsNoTracksLoop(db, 1000, fets)
+                'Catch exE As EOFException
+                '    Exit Do
+                'Catch ex As Exception
+                '    Throw ex
+                'End Try
+            Loop While fets
+        End Using
+    End Sub
+
+    Sub OmpleAlbumsByArtistsNoTracksLoop(db As p2023Entities, quants As Integer, fets As Boolean)
+        fets = False
+        Dim ss = (From t In db.TAlbum
+                  Where t.processDate Is Nothing And
+                      Not db.TTrack.Any(Function(a) a.idAlbum = t.id)).Take(quants).ToList
+        Dim c = 0
+        For Each one In ss
+            OmpleAlbumsByArtistsNoTracksOne(db, one)
+            c += 1
+            fets = True
+        Next
+    End Sub
+
+    Sub SetLabelToAlbums()
+        Dim presto As New ObjPrestoWeb
+        presto.Message = Sub(msg As String)
+                             'bt.Text = msg
+                         End Sub
+
+        presto.MsgImageDeezer = Sub(img As Image)
+                                    pb1.Image = img
+                                End Sub
+
+        presto.MsgImagePresto = Sub(img As Image)
+                                    pb2.Image = img
+                                End Sub
+
+        presto.MsgPagina = Sub(numPagina As Integer, total As Integer)
+                               lb1.Text = "Pàgina " & numPagina & " de " & total
+                               DE()
+                           End Sub
+
+        presto.MsgButton = Sub(msg As String)
+                               lb2.Text = msg
+                               DE()
+                           End Sub
+
+        presto.MsgAddAlbumFets = Sub(quants As Integer)
+                                     bt.Text = quants & " Album Fet"
+                                     System.Threading.Thread.Sleep(1000)
+                                     DE()
+                                 End Sub
+
+        presto.MsgDE = Sub()
+                           DE()
+                       End Sub
+
+        presto.DoSetLabelAlbums()
+    End Sub
+
+    Sub AlbumsPerformers()
+        Dim obj As New ObjPrestoWeb
+
+        obj.MsgDE = Sub()
+                        DE()
+                    End Sub
+
+        obj.Message = Sub(msg As String)
+                          bt.Text = msg
+                          DE()
+                      End Sub
+
+        obj.MsgArtistName = Sub(name As String)
+                                lb3.Text = name
+                                DE()
+                            End Sub
+
+        obj.MsgButton = Sub()
+
+                        End Sub
+
+        obj.MsgImageDeezer = Sub(img As Image)
+                                 pb1.Image = img
+                             End Sub
+
+        obj.MsgImagePresto = Sub(img As Image)
+                                 pb2.Image = img
+                             End Sub
+
+        obj.MsgPagina = Sub(numPagina As Integer, total As Integer)
+                            lb1.Text = "Pàgina " & numPagina & " de " & total
+                            DE()
+                        End Sub
+
+
+        obj.MsgAddTracksFets = Sub(quants As Integer)
+                                   MaterialLabel1lb4.Text = "Tracks fets: " & tracksfets
+                               End Sub
+
+        obj.MsgAddAlbumFets = Sub(quants As Integer)
+                                  albumsFets += quants
+                                  MaterialLabel1lb4.Text = "Albums fets: " & albumsFets
+                              End Sub
+
+        obj.DoFromTPerformers()
+    End Sub
+
+    Sub AlbumsPerformersLoop(quants As Integer, ByRef trobat As Boolean)
+        trobat = False
+        Dim pp = (From t In db.TPersona
+                  Where t.dataProces Is Nothing
+                  Order By t.name).Take(quants).ToList
+
+        For Each one In pp
+            AlbumsPerformersOne(db, one)
+            trobat = True
+            one.dataProces = Now
+            db.SaveChanges()
+        Next
+    End Sub
+
+    Sub AlbumsPerformersOne(db As p2023Entities, one As TPersona)
+        Dim obj As New Classica.Presto.Web.ObjPrestoWeb
+        obj.DoFromTPerformers()
+    End Sub
+
+    Sub albumCallBacks(album As ObjAlbum, one As TAlbum, db As p2023Entities)
+        album.MsgImageDeezer = Sub(img As Image)
+                                   pb1.Image = img
+                                   DE()
+                               End Sub
+
+        album.MsgImagePresto = Sub(img As Image)
+                                   pb2.Image = img
+                                   DE()
+                               End Sub
+
+        album.Message = Sub(msg As String)
+                            bt.Text = msg
+                            DE()
+                        End Sub
+
+        album.MsgNotInDeezer = Sub(msg As Boolean)
+                                   If msg Then
+                                       bt.Text = "NO TROBAT A DEEZER"
+                                   End If
+                                   one.notInDeezer = True
+                                   db.SaveChanges()
+                               End Sub
+
+    End Sub
+    Sub OmpleAlbumsByArtistsNoTracksOne(db As p2023Entities, one As TAlbum)
+        lb1.Text = one.name
+        pb1.ImageLocation = one.urlImage
+
+        Dim album As New ObjAlbum(one.urlContent)
+
+        albumCallBacks(album, one, db)
+
+        album.ReCreaAlbum()
+
+        ProgressBar1.Value += 1
+
+        DE()
+
+        one.processDate = Now
+
+        db.SaveChanges()
+
+    End Sub
+
+    Sub VerificaAlbums()
+        Using db As New Data.Repository2003.DbContextP2003
+            Dim mydb = db.db.TAlbum.Where(Function(w) w.prestige > 0)
+            ProgressBar1.Maximum = mydb.Count
+            For Each one In mydb.OrderBy(Function(p) p.prestige)
+                DoOneAlbum(one)
+            Next
+        End Using
+    End Sub
+
+    Function DoOneAlbum(t As TAlbum) As TAlbum
+        lb1.Text = t.name
+        lb2.Text = t.prestige
+        pb1.ImageLocation = t.urlImage
+        ProgressBar1.Value += 1
+        DE()
+        Return t
+    End Function
+
+    Sub DE()
+        Application.DoEvents()
+    End Sub
+
+    Private Sub frmOmple_Load(sender As Object, e As EventArgs) Handles Me.Load
+        bt.Text = Titol
+        lb1.Text = ""
+        lb2.Text = ""
+    End Sub
+End Class
